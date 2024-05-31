a75f7c76c1e371e678fa356df7608e11c5dbb4a5
GA-Purba <gabrielandrianpurba@gmail.com>
2024-05-31 23:37:04 +0700

    add func rendering list add func rendering list on products page.

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,27 +1,45 @@
 import CardProduct from "../components/Fragments/CardProduct"
 
+const products = [
+  {
+    id: 1,
+    title: "Sepatu Nike",
+    price: "1.000.000",
+    image: "./images/shoes-1.jpg",
+    desc:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores dolores, reprehenderit ipsa iure natus possimus tempore,
+    deleniti dolore dignissimos ad laborum consectetur harum hic sed fugit commodi eum rem?`,
+  },
+  {
+    id: 2,
+    title: "Sepatu Adidas",
+    price: "2.000.000",
+    image: "./images/shoes-1.jpg",
+    desc:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores dolores, reprehenderit ipsa iure natus possimus tempore,
+    deleniti dolore dignissimos ad laborum consectetur harum hic sed fugit commodi eum rem?`,
+  },
+  {
+    id: 3,
+    title: "Sepatu Puma",
+    price: "3.000.000",
+    image: "./images/shoes-1.jpg",
+    desc:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores dolores, reprehenderit ipsa iure natus possimus tempore,
+    deleniti dolore dignissimos ad laborum consectetur harum hic sed fugit commodi eum rem?`,
+  }
+]
 
 const ProductsPage = ()=>{
 return(
   <div className="flex justify-center py-5">
-    <CardProduct>
-      <CardProduct.Header ImgUrl="./images/shoes-1.jpg"/>
-      <CardProduct.Body title="Sepatu Baru">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores dolores, reprehenderit ipsa iure natus possimus tempore,
-         deleniti dolore dignissimos ad laborum consectetur harum hic sed fugit commodi eum rem?
-      </CardProduct.Body>
-      <CardProduct.Footer Price="1.000.000">
-      </CardProduct.Footer>
-    </CardProduct>
-    <CardProduct>
-      <CardProduct.Header ImgUrl="./images/shoes-1.jpg"/>
-      <CardProduct.Body title="Sepatu Baru">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores dolores, reprehenderit ipsa iure natus possimus tempore,
-         deleniti dolore dignissimos ad laborum consectetur harum hic sed fugit commodi eum rem?
-      </CardProduct.Body>
-      <CardProduct.Footer Price="1.000.000">
-      </CardProduct.Footer>
-    </CardProduct>
+    {products.map((product)=>(
+      <CardProduct key={product.id}>
+        <CardProduct.Header ImgUrl={product.image}/>
+        <CardProduct.Body title={product.title}>
+          {product.desc}
+        </CardProduct.Body>
+        <CardProduct.Footer Price={product.price}>
+        </CardProduct.Footer>
+      </CardProduct>
+    ))}
   </div>
 )
 }
